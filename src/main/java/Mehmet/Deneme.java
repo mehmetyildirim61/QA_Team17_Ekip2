e85b77fa77881c22ea81bfd8ae63dc2e21d50948
MustafaDAG33 <mustafadaggg44@gmail.com>
2022-11-17 21:29:36 +0300

    proje

--- src/main/java/Mehmet/Deneme.java
+++ src/main/java/Mehmet/Deneme.java
@@ -1,8 +1,88 @@
 package Mehmet;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
 public class Deneme {
 
-    public static void main(String[] args) {
-        System.out.println("Deneme");
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader t = new BufferedReader(new FileReader("src/main/java/Mustafa/FilmTahmin_Task"));
+
+
+        while (t.readLine() != null) {
+            System.out.println(t.readLine());
+        }
+
+
+
+
+//        List<String> film = new ArrayList<>(Arrays.asList("KELEBEK", "MUCIZE", "AYLA", "DUNKIRK", "MEMENTO", "GORA", "ESKIYA",
+//                "FIGHTCLUB", "KARAKOMIK", "KILLBILL", "AMELIE", "XMEN", "OLDBOY",
+//                "BATMAN", "KELOGLAN", "TOYSTORY", "MUTLULUK", "SEVEN"));
+//
+//
+//
+//        Scanner input = new Scanner(System.in);
+//        System.out.println("film secmek icin 0'dan 17'ye kadar bir sayi girin");
+//        int secim = input.nextInt();
+//
+//                String sectiginfilm = film.get(secim);
+//        String d = film.get(secim);
+//
+//        List<String> tahminList = new ArrayList<>(sectiginfilm.length());
+//
+//        for (int i = 0; i < sectiginfilm.length(); i++) {
+//            tahminList.add("-");
+//        }
+//        System.out.println("tahminList = " + tahminList);
+//
+//        System.out.println("sectiginiz film icin tahmin hakkiniz : " + sectiginfilm.length() * 2);
+//
+//        int tahmin=1;
+//        int tahminHakki = sectiginfilm.length() * 2;
+//        while (tahmin <= sectiginfilm.length() * 2) {
+//            Scanner scan = new Scanner(System.in);
+//            System.out.println("film tahmininiz");
+//            String ft = scan.nextLine();
+//            if (ft.equalsIgnoreCase(sectiginfilm)) {
+//                System.out.println("TEBRIKLER!!");
+//                break;
+//            }
+//            tahminHakki--;
+//
+//            System.out.println("kalan tahmin hakkiniz : " + tahminHakki);
+//
+//            System.out.println("bir harf giriniz");
+//            String c = input.next().substring(0, 1).toUpperCase();
+//
+//
+//            if (d.contains(c)) {
+//                tahminList.set(d.indexOf(c), c);
+//                System.out.println(tahminList);
+//                d= d.replaceFirst(c, " ");
+//
+//            }
+//
+//            if(tahminHakki==0){
+//                break;
+//
+//            }
+//
+//
+//        }
+//        System.out.println("KAYBETTINIZ");
+
+
+
+
+
+
     }
 }
---
+++ src/main/java/Mustafa/FilmTahminOyunu.java
@@ -0,0 +1,97 @@
+package Mustafa;
+
+import java.util.*;
+
+public class FilmTahminOyunu {
+
+    public static void main(String[] args) {
+
+        List<String> film = new ArrayList<>(Arrays.asList("KELEBEK", "MUCIZE", "AYLA", "DUNKIRK", "MEMENTO", "GORA", "ESKIYA",
+                "FIGHTCLUB", "KARAKOMIK", "KILLBILL", "AMELIE", "XMEN", "OLDBOY",
+                "BATMAN", "KELOGLAN", "TOYSTORY", "MUTLULUK", "SEVEN"));
+
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("film secmek icin 0'dan 17'ye kadar bir sayi girin");
+        int secim = input.nextInt();
+
+        filmSec(film.get(secim));
+
+    }
+
+    public static void filmSec(String str){
+
+        String d = str;//kullanicinin girdigi index'e karsilik gelen film
+
+        List<String> tahminList = new ArrayList<>(str.length());//filmin kac harfli oldugunu gosteren bos list
+        for (int i = 0; i < str.length(); i++) {
+            tahminList.add("-");
+        }
+        System.out.println("Filmin Adi = " + tahminList);//tahminList = [-, -, -, -]
+
+        System.out.println("Tahmin hakkiniz : " + (str.length() * 2) + "\n");//tahmin hakki filmin harf sayisinin iki kati kadar.
+
+        int tahmin=1;
+        int dogruTahmin=0;
+        int yanlisTahmin=0;
+
+
+        int tahminHakki = str.length() * 2;
+        while (tahmin <= str.length() * 2) {
+
+            Scanner input = new Scanner(System.in);
+            System.out.println("HARF LUTFEN!!");
+            String c = input.next().substring(0, 1).toUpperCase();
+
+
+            if (str.contains(c)) {
+                dogruTahmin++;
+                tahminList.set(str.indexOf(c), c);
+                System.out.println(tahminList);
+                System.out.println("Dogru Tahmin = " + dogruTahmin);
+                System.out.println("Yanlis Tahmin = " + yanlisTahmin);
+                str= str.replaceFirst(c, " ");
+
+            }else{
+                yanlisTahmin++;
+                System.out.println("Dogru Tahmin = " + dogruTahmin);
+                System.out.println("Yanlis Tahmin = " + yanlisTahmin);
+
+            }
+
+
+            Scanner scan = new Scanner(System.in);
+            System.out.println("Filmi tahmin et");
+            String ft = scan.nextLine();
+
+
+            if (ft.equalsIgnoreCase(d)) {
+                System.out.println("TEBRIKLER!!");
+                break;
+            }
+            tahminHakki--;
+
+            if(tahminHakki==0){
+                System.out.println("KAYBETTINIZ");
+                System.out.println("FILM : " + d);
+                break;
+
+            }
+
+            System.out.println("kalan tahmin hakkiniz : " + tahminHakki);
+
+
+
+
+
+        }
+
+    }
+
+
+
+}
+
+
+
+
